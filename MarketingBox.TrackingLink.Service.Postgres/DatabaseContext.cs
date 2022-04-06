612bc27f1b60b5817f5ab7888f268dd1530aa7ad
Lev Kurbanov <lev.k@smplt.net>
2022-04-06 22:59:26 +0500

    add postgres project

---
+++ MarketingBox.TrackingLink.Service.Postgres/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyJetWallet.Sdk.Postgres;
+
+namespace MarketingBox.TrackingLink.Service.Postgres
+{
+    public class DatabaseContext : MyDbContext
+    {
+        public const string Schema = "trackinglink-service";
+        
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (LoggerFactory != null)
+            {
+                optionsBuilder.UseLoggerFactory(LoggerFactory).EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema(Schema);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MarketingBox.TrackingLink.Service.Postgres/DesignTime/ContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using MyJetWallet.Sdk.Postgres;
+
+namespace MarketingBox.TrackingLink.Service.Postgres.DesignTime
+{
+    public class ContextFactory : MyDesignTimeContextFactory<DatabaseContext>
+    {
+        public ContextFactory() : base(options => new DatabaseContext(options))
+        {
+
+        }
+    }
+}
